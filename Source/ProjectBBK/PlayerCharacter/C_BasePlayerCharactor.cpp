82d73457de597914234ac249f0042e02f79c4a8d
yeit-ziral <qwg1447@gmail.com>
2025-10-18 05:25:26 +0900

    Movable character
    movable character without animations

--- Source/ProjectBBK/PlayerCharacter/C_BasePlayerCharactor.cpp
+++ Source/ProjectBBK/PlayerCharacter/C_BasePlayerCharactor.cpp
@@ -2,6 +2,9 @@
 
 
 #include "C_BasePlayerCharactor.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 AC_BasePlayerCharactor::AC_BasePlayerCharactor()
@@ -9,6 +12,19 @@ AC_BasePlayerCharactor::AC_BasePlayerCharactor()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	springArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	springArm->SetupAttachment(RootComponent); 
+
+	camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	camera->SetupAttachment(springArm);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
+	GetCharacterMovement()->bUseControllerDesiredRotation = false;
 }
 
 // Called when the game starts or when spawned
@@ -23,6 +39,15 @@ void AC_BasePlayerCharactor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	FVector Velocity = GetVelocity();
+	Velocity.Z = 0.0f;
+
+	if (!Velocity.IsNearlyZero())
+	{
+		FRotator TargetRotation = Velocity.Rotation();
+		FRotator NewRotation = FMath::RInterpTo(GetActorRotation(), TargetRotation, DeltaTime, 10.0f);
+		SetActorRotation(NewRotation);
+	}
 }
 
 // Called to bind functionality to input
@@ -30,5 +55,35 @@ void AC_BasePlayerCharactor::SetupPlayerInputComponent(UInputComponent* PlayerIn
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	// 1. Moving Bind (W/S & A/D)
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &AC_BasePlayerCharactor::MyMoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AC_BasePlayerCharactor::MyMoveRight);
+
+	// 2. Camera Rottation Bind (Mouse X/Y)
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &APawn::AddControllerYawInput); // APawn의 기본 함수 사용
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APawn::AddControllerPitchInput); // APawn의 기본 함수 사용
 }
 
+void AC_BasePlayerCharactor::MyMoveForward(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		FRotator Rotation = Controller->GetControlRotation();
+		FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AC_BasePlayerCharactor::MyMoveRight(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Value);
+	}
+}
--- Source/ProjectBBK/PlayerCharacter/C_BasePlayerCharactor.h
+++ Source/ProjectBBK/PlayerCharacter/C_BasePlayerCharactor.h
@@ -19,6 +19,12 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	void MyMoveForward(float Value);
+	void MyMoveRight(float Value);
+
+	void Turn(float Value);
+	void LookUp(float Value);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -26,5 +32,23 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	int a = 0;
+
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	class USpringArmComponent* springArm;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UCameraComponent* camera;
+
+	float Health = 100.0f;
+	float Stamina = 100.0f;
+
+	const float MAX_HEALTH = 100.0f;
+	const float MAX_STAMINA = 100.0f;
+
+	FVector movementInput;
+
+	// 스위칭용 (Week 1에는 구조만)
+	int32 ActiveCharacterIndex = 0;
 };
