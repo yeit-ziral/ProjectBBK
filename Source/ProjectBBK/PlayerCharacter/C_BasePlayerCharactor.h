df7a8881a497d4d1df284bdcccbca492959801ec
kky0725 <129160510+kky0725@users.noreply.github.com>
2025-10-19 00:39:28 +0900

    BP_MeleeCharacter
    MeleeCharcter에서 bluePrint로 TestSkill구현

--- Source/ProjectBBK/PlayerCharacter/C_BasePlayerCharactor.h
+++ Source/ProjectBBK/PlayerCharacter/C_BasePlayerCharactor.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Character.h"
 #include "C_BasePlayerCharactor.generated.h"
 
-UCLASS()
+UCLASS(Blueprintable)
 class PROJECTBBK_API AC_BasePlayerCharactor : public ACharacter
 {
 	GENERATED_BODY()
