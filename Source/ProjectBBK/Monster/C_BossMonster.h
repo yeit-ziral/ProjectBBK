357ed573a5d0831647ff497705a848393136f55b
skytoy12 <skytoy12@gmail.com>
2026-01-21 13:17:42 +0900

    dd
    dd

---
+++ Source/ProjectBBK/Monster/C_BossMonster.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Monster/C_BaseMonster.h"
+#include "C_BossMonster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBBK_API AC_BossMonster : public AC_BaseMonster
+{
+	GENERATED_BODY()
+	
+};
