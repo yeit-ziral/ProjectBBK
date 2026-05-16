91c3509c28d518dbac872eb948681118e7542397
kky0725 <129160510+kky0725@users.noreply.github.com>
2026-05-16 23:12:47 +0900

    RanagedUnique

---
+++ Source/ProjectBBK/Skills/C_TrapZone.cpp
@@ -0,0 +1,202 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "C_TrapZone.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/DecalComponent.h"
+#include "Components/PointLightComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "DrawDebugHelpers.h"
+#include "../Monster/C_BaseMonster.h"
+
+AC_TrapZone::AC_TrapZone()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	TriggerCapsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("TriggerCapsule"));
+	TriggerCapsule->SetCollisionProfileName(TEXT("OverlapOnlyPawn"));
+	RootComponent = TriggerCapsule;
+
+	DecalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComp"));
+	DecalComp->SetupAttachment(RootComponent);
+
+	PointLightComp = CreateDefaultSubobject<UPointLightComponent>(TEXT("PointLightComp"));
+	PointLightComp->SetupAttachment(RootComponent);
+}
+
+void AC_TrapZone::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TriggerCapsule->OnComponentBeginOverlap.AddDynamic(this, &AC_TrapZone::OnTriggerOverlap);
+}
+
+void AC_TrapZone::InitTrap(
+	UAbilitySystemComponent* InASC,
+	float InDamageMagnitude,
+	float InLifeTime,
+	float InTriggerRadius,
+	float InTriggerHalfHeight,
+	float InDamageRadius,
+	float InDamageHalfHeight)
+{
+	OwnerASC               = InASC;
+	DamageMagnitude        = InDamageMagnitude;
+	LifeTime               = InLifeTime;
+	TriggerCapsuleRadius      = InTriggerRadius;
+	TriggerCapsuleHalfHeight  = InTriggerHalfHeight;
+	DamageCapsuleRadius       = InDamageRadius;
+	DamageCapsuleHalfHeight   = InDamageHalfHeight;
+
+	TriggerCapsule->SetCapsuleSize(TriggerCapsuleRadius, TriggerCapsuleHalfHeight);
+
+	if (IsValid(DecalMaterial))
+	{
+		DecalComp->SetMaterial(0, DecalMaterial);
+	}
+
+	GetWorldTimerManager().SetTimer(
+		LifeTimerHandle,
+		this, &AC_TrapZone::OnLifeTimeExpired,
+		LifeTime, false);
+
+#if !UE_BUILD_SHIPPING
+	DrawDebugCapsule(
+		GetWorld(),
+		GetActorLocation(),
+		TriggerCapsuleHalfHeight,
+		TriggerCapsuleRadius,
+		FQuat::Identity,
+		FColor::Green,
+		false,
+		LifeTime,
+		0,
+		2.0f);
+#endif
+}
+
+void AC_TrapZone::OnTriggerOverlap(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult)
+{
+	if (bTriggered)
+	{
+		return;
+	}
+
+	if (!Cast<AC_BaseMonster>(OtherActor))
+	{
+		return;
+	}
+
+	bTriggered = true;
+
+	GetWorldTimerManager().ClearTimer(LifeTimerHandle);
+
+	DecalComp->SetVisibility(false);
+	PointLightComp->SetVisibility(false);
+
+	if (IsValid(ImpactNiagaraSystem))
+	{
+		UNiagaraComponent* NiagaraComp = UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+			GetWorld(),
+			ImpactNiagaraSystem,
+			GetActorLocation(),
+			GetActorRotation());
+
+		if (IsValid(NiagaraComp))
+		{
+			NiagaraComp->SetWorldScale3D(NiagaraScale);
+		}
+	}
+
+	ApplyDamageToOverlappingMonsters();
+
+	GetWorldTimerManager().SetTimer(
+		LifeTimerHandle,
+		this, &AC_TrapZone::DestroyTrap,
+		4.5f, false);
+}
+
+void AC_TrapZone::ApplyDamageToOverlappingMonsters()
+{
+	if (!OwnerASC.IsValid())
+	{
+		return;
+	}
+
+	if (!IsValid(DamageEffectClass))
+	{
+		return;
+	}
+
+	TArray<AActor*> OverlappedActors;
+	UKismetSystemLibrary::CapsuleOverlapActors(
+		GetWorld(),
+		GetActorLocation(),
+		DamageCapsuleRadius,
+		DamageCapsuleHalfHeight,
+		TArray<TEnumAsByte<EObjectTypeQuery>>{ ObjectTypeQuery3 },
+		AC_BaseMonster::StaticClass(),
+		TArray<AActor*>{},
+		OverlappedActors);
+
+#if !UE_BUILD_SHIPPING
+	DrawDebugCapsule(
+		GetWorld(),
+		GetActorLocation(),
+		DamageCapsuleHalfHeight,
+		DamageCapsuleRadius,
+		FQuat::Identity,
+		FColor::Red,
+		false,
+		3.0f,
+		0,
+		2.0f);
+#endif
+
+	FGameplayEffectSpecHandle SpecHandle = OwnerASC->MakeOutgoingSpec(
+		DamageEffectClass, 1.0f,
+		OwnerASC->MakeEffectContext());
+
+	if (!SpecHandle.IsValid())
+	{
+		return;
+	}
+
+	UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(
+		SpecHandle,
+		FGameplayTag::RequestGameplayTag(TEXT("Data.Damage")),
+		DamageMagnitude);
+
+	for (AActor* Actor : OverlappedActors)
+	{
+		UAbilitySystemComponent* TargetASC =
+			UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Actor);
+
+		if (IsValid(TargetASC))
+		{
+			OwnerASC->ApplyGameplayEffectSpecToTarget(*SpecHandle.Data.Get(), TargetASC);
+		}
+	}
+}
+
+void AC_TrapZone::OnLifeTimeExpired()
+{
+	if (!bTriggered)
+	{
+		DestroyTrap();
+	}
+}
+
+void AC_TrapZone::DestroyTrap()
+{
+	GetWorldTimerManager().ClearTimer(LifeTimerHandle);
+	Destroy();
+}
