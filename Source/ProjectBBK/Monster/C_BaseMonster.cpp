2886f33f76c8a96eddfbf366d870d651c5b4a5f2
yeit-ziral <qwg1447@gmail.com>
2026-05-25 16:53:08 +0900

    Update C_BaseMonster.cpp
    asfdf

--- Source/ProjectBBK/Monster/C_BaseMonster.cpp
+++ Source/ProjectBBK/Monster/C_BaseMonster.cpp
@@ -63,6 +63,16 @@ void AC_BaseMonster::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// Blueprint에 None이 직렬화된 경우 컴포넌트 목록에서 복구
+	if (!attackManager)
+		attackManager = FindComponentByClass<UC_AttackManagerComponent>();
+	if (!dataComponent)
+		dataComponent = FindComponentByClass<UC_MonsterDataComponent>();
+	if (!groggyComponent)
+		groggyComponent = FindComponentByClass<UC_GroggyComponent>();
+	if (!hpDisplayComponent)
+		hpDisplayComponent = FindComponentByClass<UC_MonsterHPDisplayComponent>();
+
 	// GAS 초기화
 	if (monsterASC)
 	{
