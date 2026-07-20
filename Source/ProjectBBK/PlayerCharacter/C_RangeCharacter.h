9bccb550c3f4545f9f93b826a27b47023fd2d9ca
yeit-ziral <qwg1447@gmail.com>
2026-07-20 22:09:58 +0900

    range attack machanism

--- Source/ProjectBBK/PlayerCharacter/C_RangeCharacter.h
+++ Source/ProjectBBK/PlayerCharacter/C_RangeCharacter.h
@@ -6,6 +6,10 @@
 #include "ProjectBBK/PlayerCharacter/C_BasePlayerCharactor.h"
 #include "C_RangeCharacter.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAmmoChangedDelegate, float, CurrentAmmo, float, MaxAmmo);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnReloadStartedDelegate, float, ReloadTime);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnReloadFinishedDelegate);
+
 UCLASS()
 class PROJECTBBK_API AC_RangeCharacter : public AC_BasePlayerCharactor
 {
@@ -14,8 +18,41 @@ class PROJECTBBK_API AC_RangeCharacter : public AC_BasePlayerCharactor
 public:
 	AC_RangeCharacter(const class FObjectInitializer &ObjectInitializer);
 
+	// 감지 범위 내 가장 가까운 살아있는 몬스터 반환 (없으면 nullptr)
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "RangeCharacter|Combat")
+	class AC_BaseMonster *GetHighestPriorityTarget() const;
+
+	// 타겟 방향으로 캐릭터를 즉시 회전 (XY 평면)
+	UFUNCTION(BlueprintCallable, Category = "RangeCharacter|Combat")
+	void FaceTarget(AActor *Target);
+
+	UFUNCTION(BlueprintPure, Category = "RangeCharacter|Ammo")
+	float GetAmmo() const;
+
+	UFUNCTION(BlueprintPure, Category = "RangeCharacter|Ammo")
+	float GetMaxAmmo() const;
+
+	UFUNCTION(BlueprintPure, Category = "RangeCharacter|Ammo")
+	float GetReloadTime() const;
+
+	UFUNCTION(BlueprintCallable, Category = "RangeCharacter|Ammo")
+	void RefillAmmo();
+
+	UFUNCTION(BlueprintCallable, Category = "RangeCharacter|Ammo")
+	void NotifyReloadStarted();
+
+	UFUNCTION(BlueprintCallable, Category = "RangeCharacter|Ammo")
+	void NotifyReloadFinished();
+
 protected:
 	virtual void BeginPlay() override;
+
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
+
+	void RegisterAmmoDelegate();
+	void OnAmmoChangedInternal(const struct FOnAttributeChangeData& Data);
+	void TryAutoReload();
 
 public:
 	virtual void Tick(float DeltaTime) override;
@@ -26,11 +63,15 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "RangeCharacter|Combat")
 	float detectionRange = 600.f;
 
-	// 감지 범위 내 가장 가까운 살아있는 몬스터 반환 (없으면 nullptr)
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "RangeCharacter|Combat")
-	class AC_BaseMonster *GetHighestPriorityTarget() const;
+	UPROPERTY(BlueprintAssignable, Category = "RangeCharacter|Ammo")
+	FOnAmmoChangedDelegate OnAmmoChanged;
 
-	// 타겟 방향으로 캐릭터를 즉시 회전 (XY 평면)
-	UFUNCTION(BlueprintCallable, Category = "RangeCharacter|Combat")
-	void FaceTarget(AActor *Target);
+	UPROPERTY(BlueprintAssignable, Category = "RangeCharacter|Ammo")
+	FOnReloadStartedDelegate OnReloadStarted;
+
+	UPROPERTY(BlueprintAssignable, Category = "RangeCharacter|Ammo")
+	FOnReloadFinishedDelegate OnReloadFinished;
+
+protected:
+	bool bAmmoDelegateRegistered = false;
 };
